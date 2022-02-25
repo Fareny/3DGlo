0f2c5a04136787b981deac553c6c0a20376a5a5f
Alex Markov <pd.fareny@gmail.com>
2022-02-25 11:45:28 +0300

    Добавил анимацию

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,60 +2,61 @@ const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
-    const imgScroll = document.querySelector("body > main > a > img");
-    const modalContent = document.querySelector("body > div.popup > div");
-    const form = document.querySelector("#form3");
-    const mainForm = document.querySelector("body > div.popup > div > div");
+    const imgScroll = document.querySelectorAll('img')[3];
+    const popupContent = document.querySelector('.popup-content');
+    const serviceBlock = document.getElementById("service-block");
+    const mainForm = popupContent.querySelector(".main-form");
 
-    console.log(mainForm);
+    let countOne = -100;
+    let countTwo = -105;
+    let countThree = 2000;
+    let idInterval;
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = `block`;
-            modal.style.left = '0px';
-            modalContent.style.top = '0px';
-            form.style.top = '300px';
-            // mainForm.style.transform = 'translateY(150%)';
+            popupContent.style.height = '360px';
 
-            animationModal();
-            animationModalContent();
-            animationMainForm();
+            if (window.screen.width < 768) {
+                console.log('Маленький экран');
+            } else {
+                animationModal();
+                animationMainForm();
+                animationModalContent();
+            }
         });
     });
 
-    let countOne = -100;
-    let countTwo = -100;
-    let countThree = 0;
-    let idInterval;
-
     const animationMainForm = () => {
-        mainForm.style.transform = 'translateY(150%)';
-        countThree++;
+        mainForm.style.position = 'fixed';
+        countThree -= 16.5;
         idInterval = requestAnimationFrame(animationMainForm);
-        // if (countThree < 100) {
-        //     mainForm.style.left = countThree * 2 + 'px';
-        // } else {
-        //     cancelAnimationFrame(idInterval);
-        //     countThree = 0;
-        // }
+        if (countThree > 20) {
+            mainForm.style.left = '33px';
+            mainForm.style.top = countThree + 'px';
+        } else {
+            cancelAnimationFrame(idInterval);
+            countThree = 2000;
+        }
     };
 
     const animationModal = () => {
+
         countTwo++;
-        idInterval = requestAnimationFrame(animationModal);
+        idInterval = requestAnimationFrame(animationModal);  //фон
         if (countTwo < 1) {
             modal.style.left = countTwo * 20 + 'px';
         } else {
             cancelAnimationFrame(idInterval);
-            countTwo = -100;
+            countTwo = -105;
         }
     };
 
     const animationModalContent = () => {
         countOne++;
-        idInterval = requestAnimationFrame(animationModalContent);
-        if (countOne < 40) {
-            modalContent.style.top = countOne * 4 + 'px';
+        idInterval = requestAnimationFrame(animationModalContent);  //Задник от от формы со строками
+        if (countOne < 19) {
+            popupContent.style.top = countOne * 5 + 'px';
         } else {
             cancelAnimationFrame(idInterval);
             countOne = -100;
@@ -66,12 +67,10 @@ const modal = () => {
         modal.style.display = `none`;
     });
 
-    let allLinks = document.querySelector("body > main > a");
-    allLinks.removeAttribute("href");
-
+    let link = document.querySelector("body > main > a");
+    link.removeAttribute("href");
     imgScroll.addEventListener('click', () => {
-        console.log('click');
-        window.scrollTo({ top: '830', behavior: 'smooth' });
+        serviceBlock.scrollIntoView({ block: "start", behavior: "smooth" });
     });
 
 };
