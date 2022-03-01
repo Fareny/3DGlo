aba0d98d57d30e2a72c288cd009d624216dab675
Alex Markov <pd.fareny@gmail.com>
2022-03-01 17:28:30 +0300

    Перенёс валидацию

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,20 +2,8 @@ const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
-    const menuItems = menu.querySelectorAll('ul>li>a');
+    const menuItems = menu.querySelectorAll('a');
     console.log(menu);
-
-    // menu.addEventListener('click', (e) => {
-    //     if (e.target.closest('.active-menu') || e.target.classList.contains('close-btn')) {
-    //         handleMenu();
-    //     } else if (e.target.matches('ul>li>a')) {
-    //         handleMenu();
-    //     } else {
-    //         if (e.target.closest('.menu')) {
-    //             handleMenu();
-    //         }
-    //     }
-    // });
 
 
     const handleMenu = () => {
@@ -23,9 +11,15 @@ const menu = () => {
     };
 
     menuBtn.addEventListener('click', handleMenu);
-    closeBtn.addEventListener('click', handleMenu);
+    // closeBtn.addEventListener('click', handleMenu);
 
-    menuItems.forEach(menuItems => { menuItems.addEventListener('click', handleMenu); });
+    menuItems.forEach(menuItems => {
+        menuItems.addEventListener('click', (e) => {
+            if (e.target.closest('menu')) {
+                handleMenu();
+            }
+        });
+    });
 
 };
 export default menu;
