36a68d7ae39eae1fb47b24b0db7288339d7ecd76
Alex Markov <pd.fareny@gmail.com>
2022-03-02 15:20:02 +0300

    Правки

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,22 +1,16 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
-    const closeBtn = menu.querySelector('.close-btn');
     const menuItems = menu.querySelectorAll('a');
-    console.log(menu);
 
-
-    const handleMenu = () => {
+    menuBtn.addEventListener('click', () => {
         menu.classList.toggle('active-menu');
-    };
-
-    menuBtn.addEventListener('click', handleMenu);
-    // closeBtn.addEventListener('click', handleMenu);
+    });
 
     menuItems.forEach(menuItems => {
         menuItems.addEventListener('click', (e) => {
             if (e.target.closest('menu')) {
-                handleMenu();
+                menu.classList.toggle('active-menu');
             }
         });
     });
